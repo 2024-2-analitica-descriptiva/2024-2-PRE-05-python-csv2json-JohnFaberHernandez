950bdcce2c09c0f8741be490f30af01d5ccf315f
JohnFaberHernandez <98713844+JohnFaberHernandez@users.noreply.github.com>
2024-11-15 19:25:59 -0500

    ConvertirCSV_Json

---
+++ homework/csv2json.py
@@ -0,0 +1,46 @@
+import csv
+import json
+"""Script para convertir un archivo CSV a JSON"""
+
+from nicegui import ui
+
+
+def convert_csv_2_json(input_file):
+    """Converts a CSV file to a JSON file"""
+
+    output_file = input_file.replace(".csv", ".json")
+    data = []
+
+    with open(input_file, "r", encoding="utf-8") as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            data.append(row)
+
+    with open(output_file, "w", encoding="utf-8") as f:
+        json.dump(data, f, indent=4)
+
+    ui.notify("The file was transformed successfully!")
+
+
+def app():
+    """Main function to run the app"""
+
+    ui.label("CSV to JSON Converter").classes("text-4xl font-bold")
+    ui.label("")
+
+    filename = ui.input(
+        label="CSV file to convert:",
+        placeholder="filename",
+    )
+
+    ui.label("")
+
+    ui.label("")
+    ui.button("Convert", on_click=lambda: convert_csv_2_json(filename.value))
+    ui.run()
+
+
+app()
+#en la terminal debo digitar el siguiete comando "pip3 isntall nicegui", 
+# luego en la terminal "homework\csv2json"
+#Luego ejecuto el programa y en navegador copio "files/drivers.json" y asi pasamos de CSV a Json
